9a9efb056ba8492a3621251bd62f45af436bd811
Yona-TYT <simus.tyt@gmail.com>
2024-01-04 18:08:12 -0400

    Update file, chg code estructure

--- gulp/watch.gulp.js
+++ gulp/watch.gulp.js
@@ -6,27 +6,38 @@ const { compilePug } = require("./pug.gulp.js");
 |  Watcher
 =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
 gulp.task("watch", () => {
-  // BrowserSync
-  browserSync.init({
-    server: { baseDir },
-    // proxy: '127.0.0.1:8010',
-    port: 443,
-    open: false, // or "local"
-    notify: false,
-    middleware: compilePug,
-  });
+	//Browsersync Task
+	function browsersyncServe(done){
+		browserSync.init({
+			server: {baseDir},
+			// proxy: '127.0.0.1:8010',
+			port: 3000,
+			open: true, // or "local"
+			notify: false,
+			middleware: compilePug,
+		});
+		//done();
+	}
 
-  const updating = (done) => {
-    browserSync.reload();
-    done();
-  };
+	function browsersyncReload(cb){
+		browsersync.reload();
+		//cb();
+	}
+	//Watch Task
+	function watchTask(){
+		console.log(paths)
+		//console.log("YonaTest: "+paths.pug.src.all+"")
+		//console.log("YonaTest: "+paths.watch.map((dir) => `${paths.dir.dev}/${dir}`))
+		gulp.watch(paths.pug.src.all, gulp.series(browsersyncReload));
+		gulp.watch(paths.style.src, gulp.series("style"));
+		// gulp.watch(gulp.series("script"));
+		gulp.watch(paths.script.src, gulp.series("script"));
+		gulp.watch(
+			paths.watch.map((dir) => `${paths.dir.dev}/${dir}`),
+			gulp.series(browsersyncReload)
+		);
+	}
 
-  gulp.watch(paths.pug.src.all, gulp.series(updating));
-  gulp.watch(paths.style.src, gulp.series("style"));
-  // gulp.watch(gulp.series("script"));
-  gulp.watch(paths.script.src, gulp.series("script"));
-  gulp.watch(
-    paths.watch.map((dir) => `${paths.dir.dev}/${dir}`),
-    gulp.series(updating)
-  );
+	browsersyncServe()
+	watchTask()
 });
--- public/assets/js/theme.js
+++ public/assets/js/theme.js
@@ -1,15 +1,13 @@
 "use strict";
 
-function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
-
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
+function ownKeys(e, r) { var t = Object.keys(e); if (Object.getOwnPropertySymbols) { var o = Object.getOwnPropertySymbols(e); r && (o = o.filter(function (r) { return Object.getOwnPropertyDescriptor(e, r).enumerable; })), t.push.apply(t, o); } return t; }
+function _objectSpread(e) { for (var r = 1; r < arguments.length; r++) { var t = null != arguments[r] ? arguments[r] : {}; r % 2 ? ownKeys(Object(t), !0).forEach(function (r) { _defineProperty(e, r, t[r]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t)) : ownKeys(Object(t)).forEach(function (r) { Object.defineProperty(e, r, Object.getOwnPropertyDescriptor(t, r)); }); } return e; }
+function _defineProperty(obj, key, value) { key = _toPropertyKey(key); if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : String(i); }
+function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
 /* -------------------------------------------------------------------------- */
-
 /*                                    Utils                                   */
-
 /* -------------------------------------------------------------------------- */
 var docReady = function docReady(fn) {
   // see if DOM is already available
@@ -19,22 +17,18 @@ var docReady = function docReady(fn) {
     setTimeout(fn, 1);
   }
 };
-
 var resize = function resize(fn) {
   return window.addEventListener('resize', fn);
 };
-
 var isIterableArray = function isIterableArray(array) {
   return Array.isArray(array) && !!array.length;
 };
-
 var camelize = function camelize(str) {
   var text = str.replace(/[-_\s.]+(.)?/g, function (_, c) {
     return c ? c.toUpperCase() : '';
   });
   return "".concat(text.substr(0, 1).toLowerCase()).concat(text.substr(1));
 };
-
 var getData = function getData(el, data) {
   try {
     return JSON.parse(el.dataset[camelize(data)]);
@@ -42,27 +36,26 @@ var getData = function getData(el, data) {
     return el.dataset[camelize(data)];
   }
 };
-/* ----------------------------- Colors function ---------------------------- */
 
+/* ----------------------------- Colors function ---------------------------- */
 
 var hexToRgb = function hexToRgb(hexValue) {
   var hex;
-  hexValue.indexOf('#') === 0 ? hex = hexValue.substring(1) : hex = hexValue; // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
-
+  hexValue.indexOf('#') === 0 ? hex = hexValue.substring(1) : hex = hexValue;
+  // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
   var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
   var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex.replace(shorthandRegex, function (m, r, g, b) {
     return r + r + g + g + b + b;
   }));
   return result ? [parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16)] : null;
 };
-
 var rgbaColor = function rgbaColor() {
   var color = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '#fff';
   var alpha = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0.5;
   return "rgba(".concat(hexToRgb(color), ", ").concat(alpha, ")");
 };
-/* --------------------------------- Colors --------------------------------- */
 
+/* --------------------------------- Colors --------------------------------- */
 
 var colors = {
   primary: '#0057FF',
@@ -89,16 +82,13 @@ var grays = {
   1100: '#0b1727',
   black: '#000'
 };
-
 var hasClass = function hasClass(el, className) {
   !el && false;
   return el.classList.value.includes(className);
 };
-
 var addClass = function addClass(el, className) {
   el.classList.add(className);
 };
-
 var getOffset = function getOffset(el) {
   var rect = el.getBoundingClientRect();
   var scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
@@ -108,26 +98,22 @@ var getOffset = function getOffset(el) {
     left: rect.left + scrollLeft
   };
 };
-
 var isScrolledIntoView = function isScrolledIntoView(el) {
   var top = el.offsetTop;
   var left = el.offsetLeft;
   var width = el.offsetWidth;
   var height = el.offsetHeight;
-
   while (el.offsetParent) {
     // eslint-disable-next-line no-param-reassign
     el = el.offsetParent;
     top += el.offsetTop;
     left += el.offsetLeft;
   }
-
   return {
     all: top >= window.pageYOffset && left >= window.pageXOffset && top + height <= window.pageYOffset + window.innerHeight && left + width <= window.pageXOffset + window.innerWidth,
     partial: top < window.pageYOffset + window.innerHeight && left < window.pageXOffset + window.innerWidth && top + height > window.pageYOffset && left + width > window.pageXOffset
   };
 };
-
 var breakpoints = {
   xs: 0,
   sm: 576,
@@ -136,33 +122,28 @@ var breakpoints = {
   xl: 1200,
   xxl: 1540
 };
-
 var getBreakpoint = function getBreakpoint(el) {
   var classes = el && el.classList.value;
   var breakpoint;
-
   if (classes) {
     breakpoint = breakpoints[classes.split(' ').filter(function (cls) {
       return cls.includes('navbar-expand-');
     }).pop().split('-').pop()];
   }
-
   return breakpoint;
 };
-/* --------------------------------- Cookie --------------------------------- */
 
+/* --------------------------------- Cookie --------------------------------- */
 
 var setCookie = function setCookie(name, value, expire) {
   var expires = new Date();
   expires.setTime(expires.getTime() + expire);
   document.cookie = "".concat(name, "=").concat(value, ";expires=").concat(expires.toUTCString());
 };
-
 var getCookie = function getCookie(name) {
   var keyValue = document.cookie.match("(^|;) ?".concat(name, "=([^;]*)(;|$)"));
   return keyValue ? keyValue[2] : keyValue;
 };
-
 var settings = {
   tinymce: {
     theme: 'oxide'
@@ -171,35 +152,32 @@ var settings = {
     borderColor: 'rgba(255, 255, 255, 0.8)'
   }
 };
+
 /* -------------------------- Chart Initialization -------------------------- */
 
 var newChart = function newChart(chart, config) {
   var ctx = chart.getContext('2d');
   return new window.Chart(ctx, config);
 };
-/* ---------------------------------- Store --------------------------------- */
 
+/* ---------------------------------- Store --------------------------------- */
 
 var getItemFromStore = function getItemFromStore(key, defaultValue) {
   var store = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : localStorage;
-
   try {
     return JSON.parse(store.getItem(key)) || defaultValue;
   } catch (_unused) {
     return store.getItem(key) || defaultValue;
   }
 };
-
 var setItemToStore = function setItemToStore(key, payload) {
   var store = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : localStorage;
   return store.setItem(key, payload);
 };
-
 var getStoreSpace = function getStoreSpace() {
   var store = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : localStorage;
   return parseFloat((escape(encodeURIComponent(JSON.stringify(store))).length / (1024 * 1024)).toFixed(2));
 };
-
 var utils = {
   docReady: docReady,
   resize: resize,
@@ -223,15 +201,14 @@ var utils = {
   setItemToStore: setItemToStore,
   getStoreSpace: getStoreSpace
 };
+
 /* -------------------------------------------------------------------------- */
-
 /*                                  Detector                                  */
-
 /* -------------------------------------------------------------------------- */
 
 var detectorInit = function detectorInit() {
   var _window = window,
-      is = _window.is;
+    is = _window.is;
   var html = document.querySelector('html');
   is.opera() && addClass(html, 'opera');
   is.mobile() && addClass(html, 'mobile');
@@ -247,11 +224,10 @@ var detectorInit = function detectorInit() {
   is.windows() && addClass(html, 'windows');
   navigator.userAgent.match('CriOS') && addClass(html, 'chrome');
 };
+
 /*-----------------------------------------------
 |   Top navigation opacity on scroll
 -----------------------------------------------*/
-
-
 var navbarInit = function navbarInit() {
   var Selector = {
     NAVBAR: '[data-navbar-on-scroll]',
@@ -271,27 +247,23 @@ var navbarInit = function navbarInit() {
     NAVBAR_ON_SCROLL: 'navbar-light-on-scroll'
   };
   var navbar = document.querySelector(Selector.NAVBAR);
-
   if (navbar) {
     var windowHeight = window.innerHeight;
     var html = document.documentElement;
     var navbarCollapse = navbar.querySelector(Selector.NAVBAR_COLLAPSE);
-
     var allColors = _objectSpread(_objectSpread({}, utils.colors), utils.grays);
-
     var name = utils.getData(navbar, DataKey.NAVBAR_ON_SCROLL);
     var colorName = Object.keys(allColors).includes(name) ? name : 'light';
     var color = allColors[colorName];
     var bgClassName = "bg-".concat(colorName);
     var shadowName = 'shadow-transition';
     var colorRgb = utils.hexToRgb(color);
-
     var _window$getComputedSt = window.getComputedStyle(navbar),
-        backgroundImage = _window$getComputedSt.backgroundImage;
-
+      backgroundImage = _window$getComputedSt.backgroundImage;
     var transition = 'background-color 0.35s ease';
-    navbar.style.backgroundImage = 'none'; // Change navbar background color on scroll
+    navbar.style.backgroundImage = 'none';
 
+    // Change navbar background color on scroll
     window.addEventListener(Events.SCROLL, function () {
       var scrollTop = html.scrollTop;
       var alpha = scrollTop / windowHeight * 5;
@@ -299,11 +271,11 @@ var navbarInit = function navbarInit() {
       navbar.style.backgroundColor = "rgba(".concat(colorRgb[0], ", ").concat(colorRgb[1], ", ").concat(colorRgb[2], ", ").concat(alpha, ")");
       navbar.style.backgroundImage = alpha > 0 || utils.hasClass(navbarCollapse, 'show') ? backgroundImage : 'none';
       alpha > 0 || utils.hasClass(navbarCollapse, 'show') ? navbar.classList.add(shadowName) : navbar.classList.remove(shadowName);
-    }); // Toggle bg class on window resize
+    });
 
+    // Toggle bg class on window resize
     utils.resize(function () {
       var breakPoint = utils.getBreakpoint(navbar);
-
       if (window.innerWidth > breakPoint) {
         navbar.style.backgroundImage = html.scrollTop ? backgroundImage : 'none';
         navbar.style.transition = 'none';
@@ -312,7 +284,6 @@ var navbarInit = function navbarInit() {
         navbar.classList.add(shadowName);
         navbar.style.backgroundImage = backgroundImage;
       }
-
       if (window.innerWidth <= breakPoint) {
         navbar.style.transition = utils.hasClass(navbarCollapse, 'show') ? transition : 'none';
       }
@@ -332,10 +303,11 @@ var navbarInit = function navbarInit() {
       navbar.style.transition = 'none';
     });
   }
-}; // /* -------------------------------------------------------------------------- */
+};
+
+// /* -------------------------------------------------------------------------- */
 // /*                            Theme Initialization                            */
 // /* -------------------------------------------------------------------------- */
-
 
 docReady(navbarInit);
 docReady(detectorInit);
